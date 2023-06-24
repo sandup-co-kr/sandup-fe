a2681c59b4c55e480beed914681db0bffaf9040d
songhwee1 <songhwee1@naver.com>
2023-06-24 12:29:45 +0900

    feat: Change login axios POST to GET
    - Change login axios POST to GET.

--- src/ services/Login/Get/login.js
+++ src/ services/Login/Get/login.js
@@ -3,9 +3,11 @@ import { BASE_URL } from "../../api";
 
 const login = async (data) => {
   try {
-    const response = await axios.post(`${BASE_URL}/login`, {
-      id: data.id,
-      pw: data.pw,
+    const response = await axios.get(`${BASE_URL}/login`, {
+      params: {
+        id: data.id,
+        pw: data.pw,
+      },
     });
     return response.data;
   } catch (error) {
