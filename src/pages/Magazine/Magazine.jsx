ba21dde816451ea443397d7d695e174c3615ae19
songhwee1 <songhwee1@naver.com>
2023-08-07 09:27:43 +0900

    feat: Add upload button to magazine page - Add upload button to magazine page.

--- src/pages/Magazine/Magazine.jsx
+++ src/pages/Magazine/Magazine.jsx
@@ -2,10 +2,31 @@ import React, { useState } from "react";
 import { useQuery } from "react-query";
 import { queryKeys } from "../../react-query/keys";
 import getUserInfo from "../../ services/Common/Get/getUserInfo";
+import { Fab } from "@mui/material";
+import styled from "@emotion/styled";
+
+const UploadImgFab = styled(Fab)`
+  width: 119px;
+  height: 53px;
+  background-color: #e4f1ff;
+  position: fixed;
+  bottom: 86px;
+  right: 6px;
+  box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0) !important;
+`;
+
+const UploadImg = styled.img`
+  width: fit-content;
+  height: fit-content;
+`;
 
 const Magazine = () => {
   const phone = localStorage.getItem("phone");
   const [userInfo, setUserInfo] = useState([]);
+
+  const handelUploadButtonClick = () => {
+    window.location.href = "/magazine/upload";
+  };
 
   const { isLoading: loadingList } = useQuery(
     [queryKeys.USER_INFO, phone],
@@ -24,9 +45,14 @@ const Magazine = () => {
     return <h1>Loading...</h1>;
   }
   return (
-    <div>
-      <h2>Magazine Page</h2>
-    </div>
+    <>
+      <div>
+        <h2>Magazine Page</h2>
+      </div>
+      <UploadImgFab variant="extended" onClick={handelUploadButtonClick}>
+        <UploadImg src={"assets/uploadButton.svg"} alt="Upload Button" />
+      </UploadImgFab>
+    </>
   );
 };
 
