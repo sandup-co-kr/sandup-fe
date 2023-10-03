f6e907fab82dc82cc6cf0c87d05a5068d93d6ad8
songhwee1 <songhwee1@naver.com>
2023-10-03 16:51:30 +0900

    fix: Img test - Img test.

--- src/components/Shop/CategoryNavigator.jsx
+++ src/components/Shop/CategoryNavigator.jsx
@@ -328,8 +328,7 @@ const CategoryNavigator = () => {
                   src={
                     activeNav === 1
                       ? `${process.env.PUBLIC_URL}/assets/healthTextClicked.svg`
-                      : process.env.PUBLIC_URL +
-                        "/assets/healthTextUnClicked.svg"
+                      : `${process.env.PUBLIC_URL}/assets/healthTextUnClicked.svg`
                   }
                   alt="Health Text1"
                 />
